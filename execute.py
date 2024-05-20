5300a9d83e64dd92c3136ae5854ba7b6f0e20e51
drifter089 <akshatmittal.0007@gmail.com>
2024-05-20 17:45:50 +0700

    adding gurobi docker images

--- execute.py
+++ execute.py
@@ -1,5 +1,18 @@
 import linopy
 
-m = linopy.read_netcdf("./input/test.nc")
-m.solve()
-m.to_netcdf("./result/results.nc")
+from linopy import Model
+m = Model()
+
+x = m.add_variables(lower=0, name='x')
+y = m.add_variables(lower=0, name='y')
+
+m.add_constraints(3*x + 7*y >= 10)
+m.add_constraints(5*x + 2*y >= 3)
+
+m.add_objective(x + 2*y)
+
+m.to_netcdf("./input/test.nc")
+
+n = linopy.read_netcdf("./input/test.nc")
+n.solve()
+n.to_netcdf("./result/results.nc")
