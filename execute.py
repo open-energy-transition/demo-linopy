f55fa42c5a2c6759c86df58bdd6c8f7017d885fe
drifter089 <akshatmittal.0007@gmail.com>
2024-06-10 09:02:45 +0700

    with updated environment

--- execute.py
+++ execute.py
@@ -1,17 +1,17 @@
 import linopy
 
 from linopy import Model
-m = Model()
+# m = Model()
 
-x = m.add_variables(lower=0, name='x')
-y = m.add_variables(lower=0, name='y')
+# x = m.add_variables(lower=0, name='x')
+# y = m.add_variables(lower=0, name='y')
 
-m.add_constraints(3*x + 7*y >= 10)
-m.add_constraints(5*x + 2*y >= 3)
+# m.add_constraints(3*x + 7*y >= 10)
+# m.add_constraints(5*x + 2*y >= 3)
 
-m.add_objective(x + 2*y)
+# m.add_objective(x + 2*y)
 
-m.to_netcdf("./input/test.nc")
+# m.to_netcdf("./input/test.nc")
 
 n = linopy.read_netcdf("./input/test.nc")
 n.solve()
